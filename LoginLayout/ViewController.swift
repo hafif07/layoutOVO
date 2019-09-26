a7a705126f9b4f13a17695009f5a18f6cba0c805
Muhammad Hafif Al Busyro <mhafif29@gmail.com>
2019-09-26 13:03:48 +0700

    layout added

--- LoginLayout/ViewController.swift
+++ LoginLayout/ViewController.swift
@@ -9,12 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var topConstraitReg: NSLayoutConstraint!
+    
+    @IBOutlet weak var topContrait: NSLayoutConstraint!
+    
+    @IBOutlet weak var ovoContrait: NSLayoutConstraint!
+    
+    @IBOutlet var splashScreen: UIView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        topConstraitReg.constant = 1000
+        topContrait.constant = 1000
+        Timer.scheduledTimer(timeInterval: 4.0 , target: self, selector:#selector(removeSP), userInfo: nil, repeats: false)
+    }
+    
+    @objc func removeSP()
+    {
+        splashScreen.removeFromSuperview()
     }
 
-
+    @IBAction func dismmisSignIn(_ sender: Any) {
+        ovoContrait.constant = 286
+        topContrait.constant = 1000
+        print("tutup")
+        UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.6 , initialSpringVelocity: 0 , options: .curveEaseOut ,animations: { self.view.layoutIfNeeded()} , completion: nil)
+        
+    }
+    @IBAction func dismisSignUp(_ sender: Any) {
+        ovoContrait.constant = 286
+        topConstraitReg.constant = 1000
+        print("tutup")
+        UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.6 , initialSpringVelocity: 0 , options: .curveEaseOut ,animations: { self.view.layoutIfNeeded()} , completion: nil)
+        
+    }
+    @IBAction func showSignIn(_ sender: Any) {
+        ovoContrait.constant = 100
+        topContrait.constant = 340
+        print("buka")
+        UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.6 , initialSpringVelocity: 0 , options: .curveEaseOut ,animations: { self.view.layoutIfNeeded()} , completion: nil)
+    }
+    @IBAction func showSignUp(_ sender: Any) {
+        ovoContrait.constant = 100
+        topConstraitReg.constant = 340
+        print("buka")
+        UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.6 , initialSpringVelocity: 0 , options: .curveEaseOut ,animations: { self.view.layoutIfNeeded()} , completion: nil)
+    }
+    
 }
 
