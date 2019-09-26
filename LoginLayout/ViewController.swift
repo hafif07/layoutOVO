fd77b0a580a091443cdd20f30300f5dd7e0b0c28
Muhammad Hafif Al Busyro <mhafif29@gmail.com>
2019-09-26 07:46:46 +0700

    Initial Commit

---
+++ LoginLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginLayout
+//
+//  Created by PondokIOS on 26/09/19.
+//  Copyright © 2019 PondokIOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
